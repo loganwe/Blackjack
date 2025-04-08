6c031fb334517d32ac645a783fdf584c7e3414b3
Logan Webb <loganjwebb13@gmail.com>
2025-04-08 19:41:56 -0400

    Tracking

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/react"
+Analytics()
 const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
 const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
